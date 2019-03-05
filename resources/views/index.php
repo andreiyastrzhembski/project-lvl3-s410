4105abd81c3e02dedda9da8603f95e7d7e1bc532
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2019-03-05 15:51:57 +0300

    update resources/views/index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -34,6 +34,19 @@
   </div>
   </nav>
 
+  <div class="jumbotron">
+    <h1 class="display-4">Page Analyzer</h1>
+    <p class="lead">Enter URL to start</p>
+    <form action="#" method="POST" class="form-inline">
+      <div class="form-group mx-sm-1">
+        <input type="text" name="url" id="inputUrl" required class="form-control">
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+    </form>
+    
+  </div>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
       integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
       crossorigin="anonymous"></script>

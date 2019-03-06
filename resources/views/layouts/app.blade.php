754c85a6b7fbd30f49d0629f22364f9b728c8ece
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2019-03-06 15:12:18 +0300

    update resources/views/

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="{{ route('domainsAll') }}">Domains<span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
